8aa633594906b5a39193e38a2a53efa7d730c94a
adorablenewbie <youngjong2965@gmail.com>
2025-07-27 12:56:30 +0900

    [ feat ] 카메라 무빙 제한 및 리더보드 1차 작업

--- Assets/Scripts/CameraMoving.cs
+++ Assets/Scripts/CameraMoving.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class CameraMoving : MonoBehaviour
 {
     [SerializeField]
-    private Camera mainCamera;
-    [SerializeField]
     private GameObject targetObject;
     [SerializeField]
     private float interpolateSpeed = 5f;
@@ -15,9 +13,28 @@ public class CameraMoving : MonoBehaviour
     private Vector3 endPosition;
     private Vector3 adjustPosition = new Vector3(0, 0, -10);
 
+    [SerializeField]
+    private float maxPositionx = 0;
+    [SerializeField]
+    private float maxPositiony = 0;
+    [SerializeField]
+    private float minPositionx = 0;
+    [SerializeField]
+    private float minPositiony = 0;
+    private float height;
+    private float width;
+
+    [SerializeField]
+    Vector2 center;
+    [SerializeField]
+    Vector2 mapSize;
+
+
     void Start()
     {
         targetObject = GameObject.Find("Player");
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
     }
 
     void Update()
@@ -27,9 +44,22 @@ public class CameraMoving : MonoBehaviour
 
     void CameraMove()
     {
-        startPosition = mainCamera.transform.position;
+        startPosition = transform.position;
         endPosition = targetObject.transform.position + adjustPosition;
-        mainCamera.transform.position = Vector3.Lerp(startPosition, endPosition, interpolateSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(startPosition, endPosition, interpolateSpeed * Time.deltaTime);
 
+        float lx = mapSize.x - width;
+        float clampX = Mathf.Clamp(transform.position.x, center.x - lx, center.x + lx);
+
+        float ly = mapSize.y - height;
+        float clampY = Mathf.Clamp(transform.position.y, center.y - ly, center.y+ ly);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, mapSize * 2);
     }
 }
